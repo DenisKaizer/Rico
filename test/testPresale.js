b14e5e4127a3b32bf7069158bd5905a6caa2b15a
Артем Егерев <artemegerev@MacBook-Pro-Artem.local>
2018-04-12 23:59:30 +0300

    s

--- test/testPresale.js
+++ test/testPresale.js
@@ -11,57 +11,51 @@ var PreSaleInstance;
 contract('TestPresale', function (accounts) {
 
     beforeEach(async () => {
-        // token
+        // Rico Token
         TokenInstance = await RicoTokenInterface.deployed({from: accounts[0]});
 
-        // presale
+        // Presale 
         var start = unix
         var period = 100000
-        PreSaleInstance = await PreSaleInterface.deployed(start, period, accounts[1], TokenInstance.address, 100);
-        console.log(PreSaleInstance.address);
+        PreSaleInstance = await PreSaleInterface.deployed(start, 
+                                                          period, 
+                                                          accounts[1], 
+                                                          TokenInstance.address, 100);
+
+        await TokenInstance.addAdmin(PreSaleInstance.address, {from: accounts[0]})
     });
 
     it("buy tokens increase balance", async () => {
-        // let oldBalance = await TokenInstance.balanceOf(accounts[1]);
-        // console.log(oldBalance);
-        console.log(accounts[1])
-        await TokenInstance.addAdmin(PreSaleInstance.address, {from: accounts[0]})
-        console.log(accounts[1])
-        await PreSaleInstance.sendTransaction({value: 1e+17, from: accounts[1]})
-        // let newBalance = await TokenInstance.balanceOf(accounts[1]);
-        // console.log(newBalance);
+        console.log(0)
+        let oldBalance = await TokenInstance.balanceOf(accounts[2]);
+        console.log(oldBalance);
+    
+        await PreSaleInstance.sendTransaction({value: 1e+17, from: accounts[2]})
+        let newBalance = await TokenInstance.balanceOf(accounts[2]);
+        
+        assert.equal((newBalance > oldBalance), true);
     })
 
-    // it("token's owner should be presaleContract", async () => {
+    it("buy tokens restrict minimum invest", async () => {
+        let oldBalance = TokenInstance.balanceOf(accounts[1]);
         
-    //     await TokenInstance.transferOwnership(PreSaleInstance.address)
-    //     let TokensOwner = await TokenInstance.owner.call();
-    //     console.log(PreSaleInstance.token.owner)
-    //     assert.equal(TokensOwner, PreSaleInstance.address);
-    // })
+        try {
+            await PreSaleInstance.sendTransaction({ value: 50, from: accounts[1] })    
+        } catch (error) {
+            err = error
+        }
+        assert.ok(err instanceof Error) 
+    })
 
-    // it("buy tokens restrict minimum invest", async () => {
-    //     let oldBalance = TokenInstance.balanceOf(accounts[1]);
-        
-    //     try {
-    //         await PreSaleInstance.sendTransaction({ value: 50, from: accounts[1] })    
-    //     } catch (error) {
-    //         err = error
-    //     }
-    //     assert.ok(err instanceof Error) 
-    // })
-
-    // it("buy tokens restrict underhardcap", async () => {
-    //     let oldBalance = TokenInstance.balanceOf(accounts[1]);
-        
-    //     await PreSaleInstance.sendTransaction({ value: 120, from: accounts[1] })    
-    //     // try {
-    //     //     await PreSaleInstance.sendTransaction({ value: 50, from: accounts[1] })    
-    //     // } catch (error) {
-    //     //     err = error
-    //     // }
-    //     // assert.ok(err instanceof Error) 
-    // })
+    it("buy tokens restrict underhardcap", async () => {
+        await PreSaleInstance.sendTransaction({value: 50 * 1e+9, from: accounts[2]});
+        try {
+            await PreSaleInstance.sendTransaction({value: 1400 * 1e+9, from: accounts[2]});
+        } catch (error) {
+            err = error
+        }
+        assert.ok(err instanceof Error) 
+    })
 
 
 
