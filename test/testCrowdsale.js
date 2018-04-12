d648f54a81c638dcf667315404044bbfeb4d65fb
Артем Егерев <artemegerev@MacBook-Pro-Artem.local>
2018-04-12 04:46:37 +0300

    first date

--- test/testCrowdsale.js
+++ test/testCrowdsale.js
@@ -1,92 +1,92 @@
 
-const BigNumber = web3.BigNumber;
+// const BigNumber = web3.BigNumber;
 
 
-const Crowdsale = artifacts.require("./Crowdsale.sol");
+// const Crowdsale = artifacts.require("./Crowdsale.sol");
 
-const Token = artifacts.require('./HyperionWattToken.sol');
-const WhiteList = artifacts.require('./WhiteList.sol');
-
-
-contract('TestCrowdsale', function (accounts) {
+// const Token = artifacts.require('./HyperionWattToken.sol');
+// const WhiteList = artifacts.require('./WhiteList.sol');
 
 
-
-    it("token's owner should be crowdsaleContract", async () => {
-        let TokenInstance = await Token.deployed();
-        await TokenInstance.transferOwnership(Crowdsale.address)
-        let TokensOwner = await TokenInstance.owner.call();
-        assert.equal(TokensOwner, Crowdsale.address);
-    })
-
-    it("whiteList and freeze should work correct", async () => {
-        let TokenInstance = await Token.deployed();
-        let CrowdsaleInstance = await Crowdsale.deployed();
-        let WhiteListInstance = await WhiteList.deployed();
-
-        try {
-            await CrowdsaleInstance.sendTransaction({ value: 1e+18, from: accounts[1] })
-            assert.fail()
-        } catch (error) {
-            err = error
-        }
-        assert.ok(err instanceof Error)
-        console.log(err)
-
-        let tokenBalance = await TokenInstance.balanceOf(accounts[1]).then(result => result.toNumber());
-        let tokenBalance1 = tokenBalance;
-        assert.equal(tokenBalance1, 0, "should be zero");
-
-        console.log(tokenBalance1)
-
-        let claimedTokens = await CrowdsaleInstance.claimedTokens(accounts[1]).then(result => result.toNumber());
-        console.log(claimedTokens)
-        let claimableTokens = await CrowdsaleInstance.claimableTokens(accounts[1]).then(result => result.toNumber());
-        console.log(claimableTokens)
-
-        await WhiteListInstance.addToWhiteList(accounts[1])
-
-        let inWL = await WhiteListInstance.isInWhiteList(accounts[1]);
-
-        assert.equal(inWL, true, "should be true");
-
-        await CrowdsaleInstance.sendTransaction({ value: 1e+18, from: accounts[1] })
+// contract('TestCrowdsale', function (accounts) {
 
 
 
-        tokenBalance = await TokenInstance.balanceOf(accounts[1]);
-        let tokenBalance2 = tokenBalance.toNumber();
-        console.log(tokenBalance2)
+//     it("token's owner should be crowdsaleContract", async () => {
+//         let TokenInstance = await Token.deployed();
+//         await TokenInstance.transferOwnership(Crowdsale.address)
+//         let TokensOwner = await TokenInstance.owner.call();
+//         assert.equal(TokensOwner, Crowdsale.address);
+//     })
 
-        claimedTokens =  await CrowdsaleInstance.claimedTokens(accounts[1]).then(result => result.toNumber());
-        console.log(claimedTokens)
-        claimableTokens =  await CrowdsaleInstance.claimableTokens(accounts[1]).then(result => result.toNumber());
-        console.log(claimableTokens)
+//     it("whiteList and freeze should work correct", async () => {
+//         let TokenInstance = await Token.deployed();
+//         let CrowdsaleInstance = await Crowdsale.deployed();
+//         let WhiteListInstance = await WhiteList.deployed();
+
+//         try {
+//             await CrowdsaleInstance.sendTransaction({ value: 1e+18, from: accounts[1] })
+//             assert.fail()
+//         } catch (error) {
+//             err = error
+//         }
+//         assert.ok(err instanceof Error)
+//         console.log(err)
+
+//         let tokenBalance = await TokenInstance.balanceOf(accounts[1]).then(result => result.toNumber());
+//         let tokenBalance1 = tokenBalance;
+//         assert.equal(tokenBalance1, 0, "should be zero");
+
+//         console.log(tokenBalance1)
+
+//         let claimedTokens = await CrowdsaleInstance.claimedTokens(accounts[1]).then(result => result.toNumber());
+//         console.log(claimedTokens)
+//         let claimableTokens = await CrowdsaleInstance.claimableTokens(accounts[1]).then(result => result.toNumber());
+//         console.log(claimableTokens)
+
+//         await WhiteListInstance.addToWhiteList(accounts[1])
+
+//         let inWL = await WhiteListInstance.isInWhiteList(accounts[1]);
+
+//         assert.equal(inWL, true, "should be true");
+
+//         await CrowdsaleInstance.sendTransaction({ value: 1e+18, from: accounts[1] })
 
 
-    })
 
-    it("claimed freeze work correct", async () => {
-        let TokenInstance = await Token.deployed();
-        let CrowdsaleInstance = await Crowdsale.deployed();
-        let WhiteListInstance = await WhiteList.deployed();
+//         tokenBalance = await TokenInstance.balanceOf(accounts[1]);
+//         let tokenBalance2 = tokenBalance.toNumber();
+//         console.log(tokenBalance2)
 
-        await WhiteListInstance.addToWhiteList(accounts[2])
-
-        await CrowdsaleInstance.sendTransaction({ value: 1e+18, from: accounts[2] })
-
-        let tokenBalance = await TokenInstance.balanceOf(accounts[2]);
-        let tokenBalance2 = tokenBalance.toNumber();
-        console.log(tokenBalance2)
-
-        claimedTokens =  await CrowdsaleInstance.claimedTokens(accounts[2]).then(result => result.toNumber());
-        console.log(claimedTokens)
-        claimableTokens =  await CrowdsaleInstance.claimableTokens(accounts[2]).then(result => result.toNumber());
-        console.log(claimableTokens)
-
-        //await CrowdsaleInstance.finishCrowdsale()
-
-    })
+//         claimedTokens =  await CrowdsaleInstance.claimedTokens(accounts[1]).then(result => result.toNumber());
+//         console.log(claimedTokens)
+//         claimableTokens =  await CrowdsaleInstance.claimableTokens(accounts[1]).then(result => result.toNumber());
+//         console.log(claimableTokens)
 
 
-})
+//     })
+
+//     it("claimed freeze work correct", async () => {
+//         let TokenInstance = await Token.deployed();
+//         let CrowdsaleInstance = await Crowdsale.deployed();
+//         let WhiteListInstance = await WhiteList.deployed();
+
+//         await WhiteListInstance.addToWhiteList(accounts[2])
+
+//         await CrowdsaleInstance.sendTransaction({ value: 1e+18, from: accounts[2] })
+
+//         let tokenBalance = await TokenInstance.balanceOf(accounts[2]);
+//         let tokenBalance2 = tokenBalance.toNumber();
+//         console.log(tokenBalance2)
+
+//         claimedTokens =  await CrowdsaleInstance.claimedTokens(accounts[2]).then(result => result.toNumber());
+//         console.log(claimedTokens)
+//         claimableTokens =  await CrowdsaleInstance.claimableTokens(accounts[2]).then(result => result.toNumber());
+//         console.log(claimableTokens)
+
+//         //await CrowdsaleInstance.finishCrowdsale()
+
+//     })
+
+
+// })
--- test/testPresale.js
+++ test/testPresale.js
@@ -1,148 +1,151 @@
-
 const BigNumber = web3.BigNumber;
 
+var unix = Math.round(+new Date()/1000);
 
-const Presale = artifacts.require("./Presale.sol");
+var PreSaleInterface = artifacts.require("./PreSale.sol");
+var RicoTokenInterface = artifacts.require("./RicoToken.sol");
 
-const Token = artifacts.require('./HyperionWattToken.sol');
-const WhiteList = artifacts.require('./WhiteList.sol');
-
+var TokenInstance;
+var PreSaleInstance;
 
 contract('TestPresale', function (accounts) {
 
+    beforeEach(async () => {
+        // token
+        TokenInstance = await RicoTokenInterface.deployed();
 
-    it("token's owner should be presaleContract", async () => {
-        let TokenInstance = await Token.deployed();
-        await TokenInstance.transferOwnership(Presale.address)
-        let TokensOwner = await TokenInstance.owner.call();
-        console.log(TokensOwner)
-        assert.equal(TokensOwner, Presale.address);
-    })
+        // presale
+        var start = unix
+        var period = 100000
+        PreSaleInstance = await PreSaleInterface.deployed(start, period, accounts[0], TokenInstance.address, 100);
+        console.log(PreSaleInstance.address);
+    });
 
-    it("whiteList should work correct", async () => {
-        let TokenInstance = await Token.deployed();
-        let PresaleInstance = await Presale.deployed();
-        let WhiteListInstance = await WhiteList.deployed();
+    // it("token's owner should be presaleContract", async () => {
         
-        let PresaleOwner = await PresaleInstance.owner.call();
-        let WLOwner = await WhiteListInstance.owner.call();
+    //     await TokenInstance.transferOwnership(PreSaleInstance.address)
+    //     let TokensOwner = await TokenInstance.owner.call();
+    //     console.log(PreSaleInstance.token.owner)
+    //     assert.equal(TokensOwner, PreSaleInstance.address);
+    // })
 
-        assert.equal(accounts[0], PresaleOwner, "Presale's Owner");
-        assert.equal(accounts[0], WLOwner, "WhiteList's Owner");
-
-        try {
-            await PresaleInstance.sendTransaction({ value: 1e+18, from: accounts[1] })
-            assert.fail()
-        } catch (error) {
-            err = error
-        }
-        assert.ok(err instanceof Error)
-        // console.log(err)
-
-        let tokenBalance = await TokenInstance.balanceOf(accounts[1]).then(result => result.toNumber());
-        let tokenBalance1 = tokenBalance;
-        assert.equal(tokenBalance1, 0, "should be zero");
-
-        console.log(tokenBalance1)
-
-        await WhiteListInstance.addToWhiteList(accounts[1])
-
-        let inWL = await WhiteListInstance.isInWhiteList(accounts[1]);
-
-        assert.equal(inWL, true, "should be true");
-
-
-        await PresaleInstance.sendTransaction({ value: 1e+18, from: accounts[1] })
-
-        tokenBalance = await TokenInstance.balanceOf(accounts[1]);
-        let tokenBalance2 = tokenBalance.toNumber();
-        console.log(tokenBalance2)
+    it("buy tokens increase balance", async () => {
+        // let oldBalance = await TokenInstance.balanceOf(accounts[1]);
+        // console.log(oldBalance);
+        console.log(accounts[1])
+        await PreSaleInstance.sendTransaction({ value: 1e+17, from: accounts[1] })
+        // let newBalance = await TokenInstance.balanceOf(accounts[1]);
+        // console.log(newBalance);
     })
 
+    // it("buy tokens restrict minimum invest", async () => {
+    //     let oldBalance = TokenInstance.balanceOf(accounts[1]);
+        
+    //     try {
+    //         await PreSaleInstance.sendTransaction({ value: 50, from: accounts[1] })    
+    //     } catch (error) {
+    //         err = error
+    //     }
+    //     assert.ok(err instanceof Error) 
+    // })
 
-    it("chck perms", async () => {
-        let TokenInstance = await Token.deployed();
-        let PresaleInstance = await Presale.deployed();
-        let WhiteListInstance = await WhiteList.deployed();
-
-        try {
-            await PresaleInstance.changePriceUSD(14692121690100,{from: accounts[1]})
-            assert.fail()
-        } catch (error) {
-            err = error
-        }
-        assert.ok(err instanceof Error)
-        // console.log(err)
-
-        try {
-            await PresaleInstance.setOracle(accounts[1], {from: accounts[1]})
-            assert.fail()
-        } catch (error) {
-            err = error
-        }
-        assert.ok(err instanceof Error)
-        // console.log(err)
-
-
-        await PresaleInstance.setOracle(accounts[1], {from: accounts[0]})
-
-        await PresaleInstance.changePriceUSD(14692121690100, {from: accounts[1]})
-
-        let priceUSD = await PresaleInstance.priceUSD.call().then(result => result.toNumber());
-
-        assert.equal(priceUSD, 14692121690100, "priceUSD");
-
-        try {
-            await PresaleInstance.manualTransfer(accounts[1], 1000,{from: accounts[1]})
-            assert.fail()
-        } catch (error) {
-            err = error
-        }
-        assert.ok(err instanceof Error)
-        // console.log(err)
-
-        await PresaleInstance.manualTransfer(accounts[1], 1000,{from: accounts[0]})
-
-        let tokenBalance = await TokenInstance.balanceOf(accounts[1]);
-        tokenBalance = tokenBalance.toNumber();
-        console.log(tokenBalance)
+    // it("buy tokens restrict underhardcap", async () => {
+    //     let oldBalance = TokenInstance.balanceOf(accounts[1]);
+        
+    //     await PreSaleInstance.sendTransaction({ value: 120, from: accounts[1] })    
+    //     // try {
+    //     //     await PreSaleInstance.sendTransaction({ value: 50, from: accounts[1] })    
+    //     // } catch (error) {
+    //     //     err = error
+    //     // }
+    //     // assert.ok(err instanceof Error) 
+    // })
 
 
-        try {
-            await PresaleInstance.setManager(accounts[1],{from: accounts[1]})
-            assert.fail()
-        } catch (error) {
-            err = error
-        }
-        assert.ok(err instanceof Error)
-        // console.log(err)
 
-        await PresaleInstance.setManager(accounts[1],{from: accounts[0]})
+    // it("chck perms", async () => {
+    //     let TokenInstance = await Token.deployed();
+    //     let PresaleInstance = await Presale.deployed();
+    //     let WhiteListInstance = await WhiteList.deployed();
 
-        await PresaleInstance.manualTransfer(accounts[1], 1000,{from: accounts[1]})
+    //     try {
+    //         await PresaleInstance.changePriceUSD(14692121690100,{from: accounts[1]})
+    //         assert.fail()
+    //     } catch (error) {
+    //         err = error
+    //     }
+    //     assert.ok(err instanceof Error)
+    //     // console.log(err)
 
-        tokenBalance = await TokenInstance.balanceOf(accounts[1]);
-        tokenBalance = tokenBalance.toNumber();
-        console.log(tokenBalance)
+    //     try {
+    //         await PresaleInstance.setOracle(accounts[1], {from: accounts[1]})
+    //         assert.fail()
+    //     } catch (error) {
+    //         err = error
+    //     }
+    //     assert.ok(err instanceof Error)
+    //     // console.log(err)
 
 
-        await PresaleInstance.manualTransfer(accounts[2], 1000,{from: accounts[1]})
+    //     await PresaleInstance.setOracle(accounts[1], {from: accounts[0]})
 
-        tokenBalance = await TokenInstance.balanceOf(accounts[2]);
-        tokenBalance = tokenBalance.toNumber();
-        console.log(tokenBalance)
+    //     await PresaleInstance.changePriceUSD(14692121690100, {from: accounts[1]})
+
+    //     let priceUSD = await PresaleInstance.priceUSD.call().then(result => result.toNumber());
+
+    //     assert.equal(priceUSD, 14692121690100, "priceUSD");
+
+    //     try {
+    //         await PresaleInstance.manualTransfer(accounts[1], 1000,{from: accounts[1]})
+    //         assert.fail()
+    //     } catch (error) {
+    //         err = error
+    //     }
+    //     assert.ok(err instanceof Error)
+    //     // console.log(err)
+
+    //     await PresaleInstance.manualTransfer(accounts[1], 1000,{from: accounts[0]})
+
+    //     let tokenBalance = await TokenInstance.balanceOf(accounts[1]);
+    //     tokenBalance = tokenBalance.toNumber();
+    //     console.log(tokenBalance)
 
 
-        try {
-            await PresaleInstance.sendTransaction({ value: 1e+18, from: accounts[2] })
-            assert.fail()
-        } catch (error) {
-            err = error
-        }
-        assert.ok(err instanceof Error)
-        // console.log(err)
+    //     try {
+    //         await PresaleInstance.setManager(accounts[1],{from: accounts[1]})
+    //         assert.fail()
+    //     } catch (error) {
+    //         err = error
+    //     }
+    //     assert.ok(err instanceof Error)
+    //     // console.log(err)
 
-    })
+    //     await PresaleInstance.setManager(accounts[1],{from: accounts[0]})
+
+    //     await PresaleInstance.manualTransfer(accounts[1], 1000,{from: accounts[1]})
+
+    //     tokenBalance = await TokenInstance.balanceOf(accounts[1]);
+    //     tokenBalance = tokenBalance.toNumber();
+    //     console.log(tokenBalance)
+
+
+    //     await PresaleInstance.manualTransfer(accounts[2], 1000,{from: accounts[1]})
+
+    //     tokenBalance = await TokenInstance.balanceOf(accounts[2]);
+    //     tokenBalance = tokenBalance.toNumber();
+    //     console.log(tokenBalance)
+
+
+    //     try {
+    //         await PresaleInstance.sendTransaction({ value: 1e+18, from: accounts[2] })
+    //         assert.fail()
+    //     } catch (error) {
+    //         err = error
+    //     }
+    //     assert.ok(err instanceof Error)
+    //     // console.log(err)
+
+    // })
 
 
 })
